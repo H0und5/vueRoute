1819e426a3239a9360a39ec00d745253b2cbf784
H0und5 <84218198+H0und5@users.noreply.github.com>
2022-09-05 16:19:59 -0500

    refactor: corrected data bind object param in the router link

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
-    <router-link :to="{ name: 'home' }">Home</router-link> |
-    <router-link :to="{ name: 'about' }">About</router-link>
+    <router-link :to="{ name: 'Home' }">Home</router-link> |
+    <router-link :to="{ name: 'About' }">About</router-link>
   </nav>
   <router-view/>
 </template>
