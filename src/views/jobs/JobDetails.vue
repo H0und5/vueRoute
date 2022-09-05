db7977530037072173711468081b371a3de8f581
H0und5 <84218198+H0und5@users.noreply.github.com>
2022-09-05 17:13:36 -0500

    docs: don't need to say this in the template, but we do have to in the object of the component

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,12 +1,16 @@
 <template>
   <h1>Job Details</h1>
-  <p>The id for this Job Details page is: {{ $route.params.id }}</p>
+  <p>The id for this Job Details page is: {{ id }}</p>
 </template>
 
 
 <script>
   export default {
-
+    data() {
+      return {
+        id: this.$route.params.id
+      }
+    }
   }
 </script>
 
